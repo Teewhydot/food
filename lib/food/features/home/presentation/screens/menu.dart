379c2175bcc471bad70f17826d4b3b4e7038d956
Tunde <tchipsical@gmail.com>
2025-05-22 17:22:22 +0100

    Add new SVG assets and update menu layout with custom scaffold

--- lib/food/features/home/presentation/screens/menu.dart
+++ lib/food/features/home/presentation/screens/menu.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food/food/components/scaffold.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FScaffold(body: Column(children: [50.verticalSpace]));
   }
 }
