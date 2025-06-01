b293bb25d80972a21f4cf47f6d93ec7c5844364d
Tunde <tchipsical@gmail.com>
2025-06-01 16:07:32 +0100

    Add transition to all routes

--- lib/food/core/routes/getx_route_module.dart
+++ lib/food/core/routes/getx_route_module.dart
@@ -19,6 +19,7 @@ import 'package:food/food/features/payments/presentation/screens/status.dart';
 import 'package:food/food/features/tracking/presentation/screens/notifications.dart';
 import 'package:food/food/features/tracking/presentation/screens/tracking.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
 
 import '../../features/home/presentation/screens/home.dart';
 import '../../features/payments/presentation/screens/cart.dart';
@@ -26,32 +27,122 @@ import '../../features/tracking/presentation/screens/call_screen.dart';
 import '../../features/tracking/presentation/screens/chat_screen.dart';
 
 class GetXRouteModule {
+  static const Transition _transition = Transition.rightToLeft;
   static final List<GetPage> routes = [
-    GetPage(name: Routes.initial, page: () => const SplashScreen()),
-    GetPage(name: Routes.onboarding, page: () => const FoodOnboarding()),
-    GetPage(name: Routes.login, page: () => const Login()),
-    GetPage(name: Routes.register, page: () => const SignUp()),
-    GetPage(name: Routes.forgotPassword, page: () => const ForgotPassword()),
-    GetPage(name: Routes.home, page: () => const Home()),
-    GetPage(name: Routes.search, page: () => const Search()),
-    GetPage(name: Routes.food, page: () => const Food()),
-    GetPage(name: Routes.foodDetails, page: () => const FoodDetails()),
+    GetPage(
+      name: Routes.initial,
+      page: () => const SplashScreen(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.onboarding,
+      page: () => const FoodOnboarding(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.login,
+      page: () => const Login(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.register,
+      page: () => const SignUp(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.forgotPassword,
+      page: () => const ForgotPassword(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.home,
+      page: () => const Home(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.search,
+      page: () => const Search(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.food,
+      page: () => const Food(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.foodDetails,
+      page: () => const FoodDetails(),
+      transition: _transition,
+    ),
     GetPage(
       name: Routes.restaurantDetails,
       page: () => const RestaurantDetails(),
+      transition: _transition,
     ),
-    GetPage(name: Routes.otpVerification, page: () => const Verification()),
-    GetPage(name: Routes.cart, page: () => const Cart()),
-    GetPage(name: Routes.paymentMethod, page: () => const PaymentMethod()),
-    GetPage(name: Routes.addCard, page: () => const AddCard()),
-    GetPage(name: Routes.statusScreen, page: () => const PaymentStatus()),
-    GetPage(name: Routes.tracking, page: () => const TrackingOrder()),
-    GetPage(name: Routes.callScreen, page: () => const CallScreen()),
-    GetPage(name: Routes.chatScreen, page: () => const ChatScreen()),
-    GetPage(name: Routes.profile, page: () => const PersonalInfo()),
-    GetPage(name: Routes.editProfile, page: () => const EditProfile()),
-    GetPage(name: Routes.address, page: () => const Address()),
-    GetPage(name: Routes.addAddress, page: () => const AddAddress()),
-    GetPage(name: Routes.notifications, page: () => const Notifications()),
+    GetPage(
+      name: Routes.otpVerification,
+      page: () => const Verification(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.cart,
+      page: () => const Cart(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.paymentMethod,
+      page: () => const PaymentMethod(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.addCard,
+      page: () => const AddCard(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.statusScreen,
+      page: () => const PaymentStatus(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.tracking,
+      page: () => const TrackingOrder(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.callScreen,
+      page: () => const CallScreen(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.chatScreen,
+      page: () => const ChatScreen(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.profile,
+      page: () => const PersonalInfo(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.editProfile,
+      page: () => const EditProfile(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.address,
+      page: () => const Address(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.addAddress,
+      page: () => const AddAddress(),
+      transition: _transition,
+    ),
+    GetPage(
+      name: Routes.notifications,
+      page: () => const Notifications(),
+      transition: _transition,
+    ),
   ];
 }
