e9bb074d6ae670bb439297dadb77557aa030619b
Tunde <tchipsical@gmail.com>
2025-06-11 13:36:18 +0100

    Initialize address and user profile databases.

--- lib/food/app/init.dart
+++ lib/food/app/init.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:food/firebase_options.dart';
+import 'package:food/food/core/services/floor_db_service/user_profile/user_profile_database_service.dart';
 
+import '../core/services/floor_db_service/address/address_database_service.dart';
 import '../core/services/floor_db_service/recent_keywords/recent_keywords_database_service.dart';
 import '../dependency_injection/set_up.dart';
 
@@ -12,6 +14,8 @@ class AppConfig {
     WidgetsFlutterBinding.ensureInitialized();
     setupDIService();
     await RecentKeywordsDatabaseService().database;
+    await AddressDatabaseService().database;
+    await UserProfileDatabaseService().database;
     // await dotenv.load(fileName: ".env");
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
