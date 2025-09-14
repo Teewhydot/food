bf7637a657bb1f25529bab6102101878a1b3ce92
Tunde <tchipsical@gmail.com>
2025-09-14 12:13:05 +0100

    feat: Refactor notifications handling to improve state management and UI rendering
    Disabled messaging for now

--- lib/food/features/tracking/presentation/screens/notifications.dart
+++ lib/food/features/tracking/presentation/screens/notifications.dart
@@ -1,17 +1,11 @@
-import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:food/food/components/scaffold.dart';
 import 'package:food/food/core/bloc/base/base_state.dart';
-import 'package:food/food/core/routes/routes.dart';
-import 'package:food/food/features/tracking/presentation/manager/chats_bloc/chats_cubit.dart';
-import 'package:food/food/features/tracking/presentation/manager/chats_bloc/chats_state.dart';
 import 'package:food/food/features/tracking/presentation/manager/notification_bloc/notification_cubit.dart';
-import 'package:food/food/features/tracking/presentation/manager/notification_bloc/notification_state.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import '../../../../components/texts.dart';
 import '../../../../core/bloc/managers/bloc_manager.dart';
@@ -19,7 +13,7 @@ import '../../../../core/constants/app_constants.dart';
 import '../../../../core/services/navigation_service/nav_config.dart';
 import '../../../../core/theme/colors.dart';
 import '../../../auth/presentation/widgets/back_widget.dart';
-import '../widgets/message_widget.dart';
+import '../../domain/entities/notification_entity.dart';
 import '../widgets/notification_widget.dart';
 
 class Notifications extends StatefulWidget {
@@ -30,165 +24,92 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
+  bool isLoading = false;
+  List<NotificationEntity> notifs = [];
   @override
   void initState() {
     super.initState();
     context.read<NotificationCubit>().loadNotifications();
-    context.read<ChatsCubit>().loadChats();
+    // context.read<ChatsCubit>().loadChats();
   }
 
   @override
   Widget build(BuildContext context) {
     final nav = GetIt.instance<NavigationService>();
 
-    return FScaffold(
-      customScroll: false,
-      appBarWidget: Row(
-        children: [
-          BackWidget(color: kGreyColor),
-          20.horizontalSpace,
-          FText(
-            text: "Notifications",
-            fontSize: 17.sp,
-            fontWeight: FontWeight.w400,
-            color: kBlackColor,
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ContainedTabBarView(
-              tabBarProperties: TabBarProperties(
-                isScrollable: false,
-                labelStyle: GoogleFonts.sen(color: kPrimaryColor),
-                indicatorSize: TabBarIndicatorSize.tab,
-                indicatorColor: kPrimaryColor,
-                labelColor: kPrimaryColor,
-                unselectedLabelStyle: GoogleFonts.sen(
-                  color: kContainerColor,
-                  fontSize: 14.sp,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              tabs: [Text("Notifications"), Text("Messages")],
-              tabBarViewProperties: TabBarViewProperties(
-                physics: BouncingScrollPhysics(),
-              ),
-              views: [
-                BlocManager<NotificationCubit, BaseState<dynamic>>(
-                  bloc: context.read<NotificationCubit>(),
-                  showLoadingIndicator: true,
-                  builder: (context, state) {
-                    if (state is LoadingState) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (state.hasData) {
-                      final notifications = state.data as List? ?? [];
-                      if (notifications.isEmpty) {
-                        return Center(
-                          child: FText(
-                            text: "No notifications available",
-                            color: kPrimaryColor,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        );
-                      }
-                      return SingleChildScrollView(
-                        child: Column(
-                          children:
-                              notifications
-                                  .map(
-                                    (notification) => NotificationWidget(
-                                      notificationEntity: notification,
-                                    ),
-                                  )
-                                  .toList(),
-                        ).paddingOnly(top: 32),
-                      );
-                    } else if (state is NotificationError) {
-                      return Center(
-                        child: FText(
-                          text: "Error loading notifications",
-                          color: kErrorColor,
-                          fontSize: 12.sp,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      );
-                    }
-                    return Center(
-                      child: FText(
-                        text: "No notifications available",
-                        color: kPrimaryColor,
-                        fontSize: 12.sp,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    );
-                  },
-                  child: const SizedBox.shrink(),
-                ),
-                BlocManager<ChatsCubit, BaseState<dynamic>>(
-                  bloc: context.read<ChatsCubit>(),
-                  showLoadingIndicator: true,
-                  builder: (context, state) {
-                    if (state is LoadingState) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (state.hasData) {
-                      final chats = state.data as List? ?? [];
-                      if (chats.isEmpty) {
-                        return Center(
-                          child: FText(
-                            text: "No messages available",
-                            color: kPrimaryColor,
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.normal,
-                          ),
-                        );
-                      }
-                      return SingleChildScrollView(
-                        child: Column(
-                          children:
-                              chats
-                                  .map(
-                                    (chat) => MessageWidget(
-                                      chat: chat,
-                                      onTap: () {
-                                        nav.navigateTo(
-                                          Routes.chatScreen,
-                                          arguments: chat,
-                                        );
-                                      },
-                                    ),
-                                  )
-                                  .toList(),
-                        ).paddingOnly(top: 32),
-                      );
-                    } else if (state is ChatsError) {
-                      return Center(
-                        child: FText(
-                          text: "Error loading messages",
-                          color: kErrorColor,
-                          fontSize: 12.sp,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      );
-                    }
-                    return Center(
-                      child: FText(
-                        text: "No messages available",
-                        color: kPrimaryColor,
-                        fontSize: 12.sp,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    );
-                  },
-                  child: const SizedBox.shrink(),
-                ),
-              ],
+    return BlocManager<NotificationCubit, BaseState<dynamic>>(
+      bloc: context.read<NotificationCubit>(),
+      showLoadingIndicator: true,
+      listener: (context, state) {
+        if (state is LoadedState) {
+          notifs = state.data as List<NotificationEntity>? ?? [];
+        }
+      },
+      child: FScaffold(
+        customScroll: false,
+        appBarWidget: Row(
+          children: [
+            BackWidget(color: kGreyColor),
+            20.horizontalSpace,
+            FText(
+              text: "Notifications",
+              fontSize: 17.sp,
+              fontWeight: FontWeight.w400,
+              color: kBlackColor,
             ),
-          ),
-        ],
-      ).paddingSymmetric(horizontal: AppConstants.defaultPadding),
+          ],
+        ),
+        // body: BlocManager<NotificationCubit, BaseState<dynamic>>(
+        //   bloc: context.read<NotificationCubit>(),
+        //   showLoadingIndicator: true,
+        //   builder: (context, state) {
+        //     if (state.hasData) {
+        //       final notifications = state.data as List? ?? [];
+        //       return SingleChildScrollView(
+        //         child: Column(
+        //           children:
+        //               notifications
+        //                   .map(
+        //                     (notification) => NotificationWidget(
+        //                       notificationEntity: notification,
+        //                     ),
+        //                   )
+        //                   .toList(),
+        //         ).paddingOnly(top: 32),
+        //       );
+        //     } else if (state is NotificationError) {
+        //       return Center(
+        //         child: FText(
+        //           text: "Error loading notifications",
+        //           color: kErrorColor,
+        //           fontSize: 12.sp,
+        //           fontWeight: FontWeight.normal,
+        //         ),
+        //       );
+        //     }
+        //     return Center(
+        //       child: FText(
+        //         text: "No notifications available",
+        //         color: kPrimaryColor,
+        //         fontSize: 12.sp,
+        //         fontWeight: FontWeight.normal,
+        //       ),
+        //     );
+        //   },
+        //   child: Column(children: [const SizedBox.shrink()]),
+        // ).paddingSymmetric(horizontal: AppConstants.defaultPadding),
+        body: SingleChildScrollView(
+          child: Column(
+            children:
+                notifs
+                    .map(
+                      (notification) =>
+                          NotificationWidget(notificationEntity: notification),
+                    )
+                    .toList(),
+          ).paddingOnly(top: 32),
+        ).paddingSymmetric(horizontal: AppConstants.defaultPadding),
+      ),
     );
   }
 }
